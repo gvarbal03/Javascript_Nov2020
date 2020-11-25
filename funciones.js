2093bb61154d14da5468f1b48380dc8d5c86cfe3
Guillermo Vargas <vargas.bag@gmail.com>
2020-11-24 21:00:59 -0600

    Adición del archivo de funciones y objetos

---
+++ funciones.js
@@ -0,0 +1,78 @@
+/*function suma(num1,num2){
+    return (num1+num2)
+
+}
+
+console.log("la usma es: ",suma(4,5))
+
+
+function cuadrado (a){
+    return a*a
+}
+
+console.log ("el cuadrado es: ", cuadrado(4,5))
+
+function myName (name){
+    return "!Hola " + name + "!"
+}
+
+console.log (myName("Memo"))
+
+
+
+//Ejercicios de entrevistra de desarrolladores
+//1. Saber si un número es par o impar
+//2. Cambiar el orden de un array (3,2,1) output (1,2,3)
+//3. ThreeCompany input: asd output aaassddd
+//4. Palindromo "ana", "oso"
+
+input casa output No es un palindromo
+input oso output si es un palindormo
+
+function parImpar(a){
+    if (a % 2 == 0){
+        return (a + " Es un número par")
+    }
+    else{
+        return (a + " Es un número impar")
+    }
+}
+
+console.log(parImpar(4))
+console.log(parImpar(8))
+console.log(parImpar(-3))
+console.log(parImpar(0))//aqui comentario
+
+
+//Cambiar el orden de un array
+
+function changeMyMind (elements){
+    var longitud = elements.length
+    var newArray = []
+
+    for (var i = 0; i < (longitud - 1); i++){
+        console.log (elements[i])
+        elements.push(elements[i])
+        newArray.unshift(elements[i])
+        console.log("Variable: ", i)
+
+    }
+    return newArray
+}
+
+var array = [3,17,25,1,50,-3,11]
+console.log (changeMyMind(array))*/
+
+//3. ThreeCompany input: asd output aaasssddd
+
+function three(cadena){
+    var n = cadena.lenght
+    var output
+    
+    for (var i=0; i<n;i++){
+        var letra = cadena
+
+    }
+}
+
+console.log (three('casa'))
---
+++ objetos.js
@@ -0,0 +1,18 @@
+var persona = {
+    nombre : "Memo",
+    id : 1124
+}
+
+persona.edad = 30
+
+console.log(persona)
+console.log(persona.edad)
+
+
+var coche = {
+    color : "blanco", 
+    marca : "BMW",
+    llantas: 3
+}
+
+console.log(coche.edad)
